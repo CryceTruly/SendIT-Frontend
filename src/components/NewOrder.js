9946e8fdb2ab82c814a8540a5536b8c6dde603a2
crycetruly <crycetruly@gmail.com>
2019-04-26 06:34:58 +0300

    ft(createParcels): Users should create parcel requests
    - Users should create parcel requests
    
    [Delivers #165116852]

--- src/components/NewOrder.js
+++ src/components/NewOrder.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react'
-import {
-    Button,
-    Card,
-    CardBody,
-    CardTitle,
-    Form,
-    FormGroup,
-    Label,
-    Input,
-    Alert,
-    Row,
-    Col
-  } from 'reactstrap';
-export default class NewOrder extends Component {
+import {connect} from 'react-redux';
+import {createParcel} from "../actions/parcelActions";
+import {Card,CardBody,CardTitle,Row,Form,FormGroup,Label,Input,Button,Alert} from "reactstrap";
+export class NewOrder extends Component {
     state = {
-        fullname: '',
-        email: '',
-        password: '',
+        pickup: '',
+        destination: '',
+        weight: 0,
+        qty:0,
         phone_number:'',
-        username:'',
-        msg: null
+        rfullName:'',
+        remail:'',
+        msg: null,
+        description:""
       };
+
+
+
+      onChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+      };
+
+
+
+
+
+
+    onSubmit = e => {
+      e.preventDefault();
+      const { rfullName,destination,pickup, weight, qty,phone_number ,remail,description} = this.state;
+
+      const NewOrder = {
+        recipient_email:remail,
+        weight:parseInt(weight),
+        pickup_address:pickup,
+        destination_address:destination,
+        quantity:parseInt(qty),
+        parcel_description:description,
+        recipient_phone_number:phone_number,
+        recipient_name:rfullName
+      };
+
+      // Attempt to create
+      this.props.createParcel(NewOrder,this.props);
+    };
+
+
+
 
   render() {
     return (
@@ -35,19 +61,18 @@ export default class NewOrder extends Component {
 
             <Form onSubmit={this.onSubmit}>
 
-
-{this.state.msg?<Alert color="danger">{this.state.msg}</Alert>:null}
+{this.props.errors.id==="ADD_PARCEL_FAIL"?(<Alert className="alert-danger">{this.props.errors.msg}</Alert>):""}
 
             <Row>
               <div className="col-md-6">
 
               <FormGroup>
-                <Label for='fullname'>Pickup Location</Label>
+                <Label for='pickup'>Pickup Location</Label>
                 <Input
                   type='text'
-                  name='fullname'
-                  id='fullname'
-                  placeholder='Full Name' required
+                  name='pickup'
+                  id='pickup'
+                  placeholder='Pickup location' required
                   className='mb-3'
                   onChange={this.onChange}
                 />
@@ -55,12 +80,12 @@ export default class NewOrder extends Component {
 
 
 <FormGroup>
-                <Label for='email'>Destination address</Label>
+                <Label for='destination'>Destination address</Label>
                 <Input
-                  type='email'
-                  name='email'
-                  id='email' required
-                  placeholder='Email'
+                  type='text'
+                  name='destination'
+                  id='destination' required
+                  placeholder='Destination LOcation'
                   className='mb-3'
                   onChange={this.onChange}/>
                  </FormGroup>
@@ -75,49 +100,83 @@ export default class NewOrder extends Component {
                   onChange={this.onChange}
                 />
                 </FormGroup>
+                <FormGroup>
+<Label for='remail'>Recipient Email</Label>
+                <Input
+                  type='email'
+                  name='remail'
+                  id='remail' required
+                  placeholder='Email'
+                  className='mb-3'
+                  onChange={this.onChange}
+                />
+                </FormGroup>
 
               </div>
                <div className="col-md-6">
 <FormGroup>
-  <Label for='username'>Recipient Fullname</Label>
+  <Label for='rfullName'>Recipient Fullname</Label>
                 <Input
                   type='text'
-                  name='username'
-                  id='username' required
+                  name='rfullName'
+                  id='rfullName' required
                   placeholder=''
                   className='mb-3'
                   onChange={this.onChange}
                 />
-</FormGroup>
-<FormGroup>
-   <Label for='password'>Parcel Description</Label>
-                <Input
-                  type='password'
-                  name='password' required
-                  id='password'
-                  placeholder=''
-                  className='mb-3'
-                  onChange={this.onChange}
-                />
-
-
-
 </FormGroup>
 
 <FormGroup>
-
-                  <Label for='password_comfirm'>Weight </Label>
+   <Label for='description'>Parcel Description</Label>
                 <Input
-                  type='password_comfirm'
-                  name='password'
-                  id='password_comfirm' required
+                  type='description'
+                  name='description' required
+                  id='description'
+                  placeholder=''
+                  className='mb-3'
+                  onChange={this.onChange}
+                />
+
+
+
+</FormGroup>
+
+<FormGroup>
+<FormGroup>
+
+<Label for='qty'>Quantity </Label>
+<Input
+type='number'
+name='qty'
+id='qty' required
+placeholder=''
+className='mb-3'
+onChange={this.onChange}
+/>
+</FormGroup>
+
+                  <Label for='weight'>Weight </Label>
+                <Input
+                  type='number'
+                  name='weight'
+                  id='weight' required
                   placeholder=''
                   className='mb-3'
                   onChange={this.onChange}
                 />
 </FormGroup>
- <Button color='dark' style={{ marginTop: '2rem' }} block>
-                  Submit
+{
+  this.props.parcels.isAdding?(
+    <div className="d-flex text-primary justify-content-center">
+    <div className="spinner-border" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  </div>
+  ):null
+}
+ <Button color='dark' disabled={this.props.parcels.isAdding} style={{ marginTop: '2rem' }} block>
+ {
+  this.props.parcels.isAdding? "Please wait":"Submit"}
                 </Button>
 
 
@@ -139,3 +198,10 @@ export default class NewOrder extends Component {
     )
   }
 }
+
+const mapStateToProps=state=>({
+  errors:state.errors,
+  parcels:state.parcels
+})
+
+export default connect(mapStateToProps,{createParcel})(NewOrder)
